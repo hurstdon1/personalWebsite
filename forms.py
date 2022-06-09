15865ecf3f3803a4fbbc417021a2f89358f38288
Don Hurst <hurstdon1@gmail.com>
2022-06-09 16:20:14 +0000

    Fixed issue with website not properly working with gunicorn. Uninstalled server module and reinstalled within venv

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, SubmitField, validators
+from wtforms import StringField, SubmitField, validators
 from wtforms.validators import InputRequired
 
 class ContactForm(FlaskForm):
 
-    name = TextField("Name", validators=[InputRequired()])
-    email = TextField("Email", validators=[InputRequired()])
-    subject = TextField("Subject")
-    message = TextField("Message") 
+    name = StringField("Name", validators=[InputRequired()])
+    email = StringField("Email", validators=[InputRequired()])
+    subject = StringField("Subject")
+    message = StringField("Message") 
     submit = SubmitField("send")
     
